825300dc24e6eb9aa40547066bf3603d42592eff
oabdumavlanov-pixel <oabdumavlanov@ucsd.edu>
2026-05-19 08:49:25 -0700

    Update 02_window_functions.sql

--- sql/02_window_functions.sql
+++ sql/02_window_functions.sql
@@ -14,3 +14,62 @@ SELECT
   ) AS gdp_change
 
 FROM world_bank_migration_data;
+
+#row numbers
+SELECT employee, department, salary
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY department
+               ORDER BY salary DESC
+           ) AS rn
+    FROM employees
+) t
+WHERE rn = 1;
+
+🔹 2. RANK vs DENSE_RANK
+Question:
+
+Rank employees by salary.
+
+SELECT employee,
+       salary,
+       RANK() OVER (ORDER BY salary DESC) AS rnk,
+       DENSE_RANK() OVER (ORDER BY salary DESC) AS dense_rnk
+FROM employees;
+
+🔹 3. Running Total
+Question:
+
+Calculate cumulative sales.
+
+SELECT order_date,
+       sales,
+       SUM(sales) OVER (
+           ORDER BY order_date
+       ) AS running_total
+FROM orders;
+
+🔹 4. LAG()
+Question:
+
+Compare today's sales with yesterday's.
+
+SELECT order_date,
+       sales,
+       LAG(sales) OVER (
+           ORDER BY order_date
+       ) AS previous_day_sales
+FROM orders;
+
+🔹 5. LEAD()
+Question:
+
+Find next month's revenue.
+
+SELECT month,
+       revenue,
+       LEAD(revenue) OVER (
+           ORDER BY month
+       ) AS next_month_revenue
+FROM revenue_table;
